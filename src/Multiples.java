8afa37bf42a0b750f6878d29ce34ac3dfc16f36c
Alex Fu <fzrrralex@Alexs-MacBook-Pro.local>
2023-09-13 01:10:00 -0400

    update multiples

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,13 +1,21 @@
 public class Multiples {
-    public static void main(String[] args) {
-        int number = 0;
-        for(int i = 1; i < 1000; i++ ) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-            if (divisibleBy3 || divisibleBy5){
-                number++;
+    public static int main(int limit, int m1, int m2){
+        int count = 0;
+        for (int i = 1; i < limit; i++){
+            if (i % m1 == 0 || i % m2 == 0){
+                count++;
+
             }
         }
-        System.out.println(number);
+        return count;
+    }
+
+    public static int main() {
+        return main(1000, 3, 5);
+    }
+
+    public static void main(String[] args) {
+        int count = main();
+        System.out.println(count);
     }
 }
